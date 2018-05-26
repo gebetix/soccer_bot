68a551ce4a0ba78e04e6feea9343fe7fe2a529a2
Aleksandr Bukin <gebetix@gmail.com>
2018-05-26 14:44:11 +0500

    add game.py

---
+++ game.py
@@ -0,0 +1,28 @@
+from s3 import S3
+
+class Game(object):
+    def __init__(self, date, place):
+        self.date = date
+        self.place = place
+
+    def add_player(self, username, chat_id):
+        s3 = S3()
+        key = 'games/' + self.date + '/' + username
+        try:
+             s3.client.get_object(Bucket='soccer-storage', Key=key)
+             message = 'Рад твоему рвению, но записаться можно только один раз.'
+        except:
+             s3.client.put_object(Bucket='soccer-storage', Key=key, Body=str(chat_id))
+             message = 'Отлично! Я внёс тебя в состав на игру.'
+        return message
+
+    def del_player(self, username):
+        s3 = S3()
+        key = 'games/' + self.date + '/' + username
+        try:
+             s3.client.get_object(Bucket='soccer-storage', Key=key)
+             s3.client.delete_object(Bucket='soccer-storage', Key=key)
+             message = 'С глубоким сожалением вычёркиваю тебя из состава на игру.'
+        except:
+             message = 'Убрать из состава не могу – тебя в нём и так не было.'
+        return message
