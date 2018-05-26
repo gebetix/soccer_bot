80f1edb5612b1716c1e04fda4927ec1a98c57345
Aleksandr Bukin <gebetix@gmail.com>
2018-05-26 14:59:41 +0500

    fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 
 from config import config
 from game import Game
-from telegram.ext import Updater, RegexHandler, CommandHandler, filters.Filters as Filters
+from telegram.ext import Updater, RegexHandler, CommandHandler
 from telegram import TelegramError, ReplyKeyboardMarkup
 
 logging.basicConfig(level=logging.DEBUG,
@@ -48,9 +48,9 @@ def location(bot, update):
 updater = Updater(token=config['token'])
 
 start_handler = CommandHandler('start', start)
-add_me_handler = RegexHandler('^(Записаться)$', add_me)
-del_me_handler = RegexHandler('^(Отменить запись)$', del_me)
-location_handler = RegexHandler('^(Где играем?)$', location)
+add_me_handler = RegexHandler('^(Записаться)$', add_me, pass_user_data=True)
+del_me_handler = RegexHandler('^(Отменить запись)$', del_me, pass_user_data=True)
+location_handler = RegexHandler('^(Где играем?)$', location, pass_user_data=True)
 
 updater.dispatcher.add_handler(start_handler)
 updater.dispatcher.add_handler(add_me_handler)
